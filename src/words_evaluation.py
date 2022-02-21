dc0ff7c6fc83f913a4868466a125d69ef0853e84
VanderSant <vanderson.santos@usp.br>
2022-02-21 02:25:31 -0300

    ⚡️ add colors parameters

--- src/words_evaluation.py
+++ src/words_evaluation.py
@@ -7,19 +7,29 @@ import pandas as pd
 WORDLE_POSSIBLE_WORDS_PATH = "docs/wordle/all_possibles_words.txt"
 WORDLE_ALL_WORDS_PATH = "docs/wordle/all_words.txt"
 
-def evaluate_step(word,all_words):
+WORDLE_GREEN_COLOR_SIMBOL = '2'
+WORDLE_YELLOW_COLOR_SIMBOL = '1'
+
+def evaluate_step(word,solutions):
     right_pos,in_word = 0,0
-    for word_i in all_words:
+
+    for solution in solutions:
         is_in_right_pos,is_char_in_word = 0,0
+        solution_list = list(solution) 
         for (i,char_word) in enumerate(word):
-            if(char_word == word_i[i]):
+            if(char_word == solution_list[i]):
                 is_in_right_pos = 1
-            elif(char_word in word_i):
+                solution_list[i] = WORDLE_GREEN_COLOR_SIMBOL
+            elif(char_word in solution_list):
                 is_char_in_word = 1
+                pos_char_word = solution_list.index(char_word)
+                solution_list[pos_char_word] = WORDLE_YELLOW_COLOR_SIMBOL
+            print(word)
+            print(solution_list)
         right_pos += is_in_right_pos
         in_word += is_char_in_word
-    good_option = right_pos + in_word 
-    return right_pos,in_word,good_option
+    exist_right_in_word = right_pos + in_word 
+    return right_pos,in_word,exist_right_in_word
 
 def evaluate():
     with open(WORDLE_POSSIBLE_WORDS_PATH) as f:
@@ -30,14 +40,14 @@ def evaluate():
         all_words_list = [line.rstrip('\n') for line in f]
     all_words = np.array(all_words_list)
 
-    rank_words = pd.DataFrame(columns=['word','right_pos','has_in','good_option'])
+    rank_words = pd.DataFrame(columns=['word','right_pos','exist_in_word','exist_right_in_word'])
 
     for word in all_words:
         eright_pos,ein_word,has_color = evaluate_step(word,possi_words)
-        rank_words = rank_words.append({'word':word,'right_pos':eright_pos,'has_in':ein_word,'good_option':has_color}, ignore_index=True)
+        rank_words = rank_words.append({'word':word,'right_pos':eright_pos,'exist_in_word':ein_word,'exist_right_in_word':has_color}, ignore_index=True)
 
     # tranform dataframe to csv file
-    rank_words = rank_words.sort_values(by=['good_option'],ascending=False)
+    rank_words = rank_words.sort_values(by=['exist_right_in_word'],ascending=False)
     rank_words.to_csv('rank_wordle_words.csv',index=False)
 
 if __name__ == "__main__":
